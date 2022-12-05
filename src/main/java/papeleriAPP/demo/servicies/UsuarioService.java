81c71a4c8f8b8e5c6b6b7f391a6e87ee4e4b7f2a
Fredy Gonzalez <fredylingling.fgg6@gmail.com>
2022-12-05 17:41:51 -0500

    se organizo el codigo

--- src/main/java/papeleriAPP/demo/servicies/UsuarioService.java
+++ src/main/java/papeleriAPP/demo/servicies/UsuarioService.java
@@ -1,14 +1,8 @@
 package papeleriAPP.demo.servicies;
-
 import java.util.ArrayList;
 import java.util.Optional;
-
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import papeleriAPP.demo.models.UsuarioModels;
 import papeleriAPP.demo.repositories.UsuarioRepositorie;
 
@@ -19,23 +13,23 @@ public class UsuarioService {
 
     UsuarioRepositorie usuarioRepositorie;
 
-    public ArrayList<UsuarioModels> obtenerUsuarios(){
-        return (ArrayList<UsuarioModels>)usuarioRepositorie.findAll();
+    public ArrayList < UsuarioModels > obtenerUsuarios() {
+        return (ArrayList < UsuarioModels > ) usuarioRepositorie.findAll();
     }
 
-    public UsuarioModels guardarUsuario(UsuarioModels usuario){
+    public UsuarioModels guardarUsuario(UsuarioModels usuario) {
         return usuarioRepositorie.save(usuario);
     }
 
-    public Optional<UsuarioModels> obtenerPorId(long id){
+    public Optional < UsuarioModels > obtenerPorId(long id) {
         return usuarioRepositorie.findById(id);
     }
 
-    public ArrayList <UsuarioModels> obtenerPorProridad(Integer prioridad){
+    public ArrayList < UsuarioModels > obtenerPorProridad(Integer prioridad) {
         return usuarioRepositorie.findByPrioridad(prioridad);
     }
 
-    public boolean eliminarUsuario(Long id){
+    public boolean eliminarUsuario(Long id) {
 
         try {
             usuarioRepositorie.deleteById(id);
@@ -45,5 +39,5 @@ public class UsuarioService {
         }
     }
 
-    
+
 }
